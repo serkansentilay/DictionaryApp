9660b08c7bf2132df3176b47778eda856f7c6cc6
Serkan Sentilay <sentiilay@gmail.com>
2023-08-25 12:26:28 +0300

    installed redux

---
+++ src/components/Word/Word.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+import styles from "./Word.style"
+
+const Word = () => {
+    return (
+        <View>
+            <Text>Word</Text>
+        </View>
+    )
+}
+
+export default Word
